58f1368c6c741c7f534c5cf77d94896a6d8f31cd
Ranjithvavilla134 <96306788+Ranjithvavilla134@users.noreply.github.com>
2025-06-05 19:39:09 +0530

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,35 +1,23 @@
 <?php
 
-// comment out the following two lines when deployed to production
-if (getenv('HTTP_WS') === 'lab')
-{
-    $debug = true;
-    $environment = 'dev';
-    $config_file = '/../config/lab/lab.php';
-}
-elseif (getenv('APP_ENV') === 'test')
-{
-    $debug = true;
-    $environment = 'test';
-    $config_file = '/../config/test/test.php';
-}
-else
-{
-    $debug = false;
-    $environment = 'prod';
-    $config_file = '/../config/prod/prod.php';
-}
+// Force Yii2 into debug/dev mode for testing purposes
+$debug = true;
+$environment = 'dev';
+$config_file = '/../config/test/test.php';  // Make sure this file exists
 
 defined('YII_WEB_APP') or define('YII_WEB_APP', true);
 defined('YII_DEBUG') or define('YII_DEBUG', $debug);
 defined('YII_ENV') or define('YII_ENV', $environment);
 
+// Autoload and Yii core
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+// Merge main config with environment-specific config
 $config = yii\helpers\ArrayHelper::merge(
     require(__DIR__ . '/../config/main.php'),
     require(__DIR__ . $config_file)
 );
 
+// Run Yii2 app
 (new yii\web\Application($config))->run();
